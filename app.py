6eb440c199c887b1c1e19ee38f906e34aa6f2fb6
Stefu Alexandru <alexstefu112@yahoo.com>
2025-09-03 19:30:54 +0300

    Modificare mesaj

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "Hello World Update"
+    return "Hello World Update again"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
