278fce4609f8e40c116eb90db2469e54163252e5
Stefu Alexandru <alexstefu112@yahoo.com>
2025-09-03 19:51:01 +0300

    Commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "DevOps Simple Project"
+    return "DevOps Project: CI/CD, Docker, Render Deployment"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
